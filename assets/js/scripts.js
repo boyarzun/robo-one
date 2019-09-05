0b8304abfb0af2699b277740c17e11b2adcfb7b1
Byron Oyarzún <b.oyarzn@gmail.com>
2019-09-04 22:38:21 -0400

    Update partners

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -332,3 +332,26 @@ function sendContactForm(event) {
 	})
 
 }
+
+$(document).ready(function(){
+	$('.customer-logos').slick({
+		slidesToShow: 4,
+		slidesToScroll: 1,
+		autoplay: true,
+		autoplaySpeed: 1000,
+		arrows: false,
+		dots: false,
+			pauseOnHover: false,
+			responsive: [{
+			breakpoint: 768,
+			settings: {
+				slidesToShow: 3
+			}
+		}, {
+			breakpoint: 520,
+			settings: {
+				slidesToShow: 2
+			}
+		}]
+	});
+});
