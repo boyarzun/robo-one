6637b5dfb8155fef681468bbf55e83ce53c6a9d9
Javier Oyarzún <b.oyarzn@gmail.com>
2019-09-06 01:44:10 -0400

    Update keywords

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -332,26 +332,3 @@ function sendContactForm(event) {
 	})
 
 }
-
-$(document).ready(function(){
-	$('.customer-logos').slick({
-		slidesToShow: 4,
-		slidesToScroll: 1,
-		autoplay: true,
-		autoplaySpeed: 1000,
-		arrows: true,
-		dots: false,
-			pauseOnHover: false,
-			responsive: [{
-			breakpoint: 768,
-			settings: {
-				slidesToShow: 3
-			}
-		}, {
-			breakpoint: 520,
-			settings: {
-				slidesToShow: 2
-			}
-		}]
-	});
-});
