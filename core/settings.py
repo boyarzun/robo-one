b2716d22e0d28986c62086252dd1db1bba267475
Byron Oyarzún <b.oyarzn@gmail.com>
2020-10-19 20:48:06 -0300

    fixed collectstatic

--- core/settings.py
+++ core/settings.py
@@ -55,7 +55,7 @@ INSTALLED_APPS = [
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
-    "django.contrib.staticfiles",
+    "core.staticfiles.StaticFilesConfig",
     "registration",
     "pages",
 ]
@@ -111,9 +111,15 @@ AUTH_PASSWORD_VALIDATORS = [
     {
         "NAME": "django.contrib.auth.password_validation.UserAttributeSimilarityValidator",
     },
-    {"NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",},
-    {"NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",},
-    {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",},
+    {
+        "NAME": "django.contrib.auth.password_validation.MinimumLengthValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.CommonPasswordValidator",
+    },
+    {
+        "NAME": "django.contrib.auth.password_validation.NumericPasswordValidator",
+    },
 ]
 
 
---
+++ core/staticfiles.py
@@ -0,0 +1,5 @@
+from django.contrib.staticfiles.apps import StaticFilesConfig
+
+
+class StaticFilesConfig(StaticFilesConfig):
+    ignore_patterns = ["CVS", ".*", "*~", "*.js", "*.css"]
